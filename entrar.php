28ed02951fca3263376a682eb1d9b9641ec089a8
Rony-Peterson-bit <rpetsil@gmail.com>
2020-10-24 03:52:58 -0200

    update entrar

--- entrar.php
+++ entrar.php
@@ -2,27 +2,36 @@
  // Header
 include_once 'includes/header.php';
 ?>
-	<h1>Acesse a sua conta</h1>
+	<h1 style="text-align: center;">Acesse a sua conta</h1>
 	<div class="row">
 	<form method="POST" class="col s12 m12 l12">
 		<div class="row">
+
 		   <div class="input-field col s4 offset-s4">
-			 <i class="material-icons prefix">account_circle</i>
-			 <input type="email" name="email" autocomplete="off" maxlength="40">
-			 <label for="icon_prefix">Email</label>
+			 <i class="material-icons prefix">local_post_office</i>
+			 <input id="email" type="email" name="email" autocomplete="off" maxlength="40"class="validate" >
+			 <label for="email" data-error="wrong" data-success="right">Email</label>
 		   </div>
 		   
 		   <div class="input-field col s4  offset-s4  ">
-		      <i class="material-icons prefix">phone</i>
+		      <i class="material-icons prefix">lock</i>
 			  <input type="password" name="senha">
 			  <label for="icon_telephone">Senha</label> 
 		   </div>
 
 		   <div class="input-field col s4  offset-s5  ">
-				<input type="submit" value="ENTRAR">
-				<a href="cadastrar.php">Registre-se agora!</a>
+		  	 <button class="btn waves-effect waves-light" type="submit" 			name="action">Entrar
+   			 <i class="material-icons right">send</i>
+  			</button>
 		   </div>
-		
+
+		   <div class="input-field col s4  offset-s5  ">
+			   <a href="cadastrar.php">Registre-se agora!</a>
+			   <a href="cadastrar.php" class="btn btn-floating btn-small cyan pulse"><i class="small material-icons">edit</i></a>
+
+			   
+
+		   </div>
 		</div>
 		
 	</form>
@@ -44,11 +53,11 @@ if(isset($_POST['email']))
 			header("location: index.php");
 		}else
 		{
-			?><h1>Email e/ou senha estão incorretos!</h1><?php
+			?><h1 style="text-align: center;">Email e/ou senha estão incorretos!</h1><?php
 		}
 	}else
 	{
-	   ?>	<h1>Preencha todos os campos! </h1> 
+	   ?>	<h1 style="text-align: center;">Preencha todos os campos! </h1> 
 	   <?php
 	}
 }
